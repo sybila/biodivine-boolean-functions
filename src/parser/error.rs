37f2c460404a7e9b4097bf9b4c106098586d0b48
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2023-11-15 22:50:55 +0100

    feat(parser): added ParseTokens and general Parse error

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,10 +1,4 @@
 #[derive(Debug, thiserror::Error)]
-pub enum TokenParseError {
-    #[error("Unknown symbol encountered {0}")]
-    UnknownSymbol(String),
-}
-
-#[derive(Debug, thiserror::Error)]
 pub enum TokenizeError {
     #[error("Unexpected `)` encountered")]
     UnexpectedClosingParenthesis,
@@ -14,4 +8,20 @@ pub enum TokenizeError {
     UnexpectedClosingCurlyBrace,
     #[error("Unexpected whitespace encountered in the middle of operator")]
     UnexpectedWhitespace,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseTokensError {
+    #[error("Operator had nothing as its operand")]
+    EmptySideOfOperator,
+    #[error("Encountered terminal that was too long")]
+    TerminalTooLong,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ParseError {
+    #[error(transparent)]
+    TokenizingError(#[from] TokenizeError),
+    #[error(transparent)]
+    ParsingError(#[from] ParseTokensError),
 }

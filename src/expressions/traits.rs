dffbed0acdc9eb0765793f10005b7e202b0986db
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-03-22 11:59:48 +0100

    feat(expressions): implemented Display for Expression

--- src/expressions/traits.rs
+++ src/expressions/traits.rs
@@ -107,3 +107,20 @@ impl Parse for Expression<String> {
         Ok(parsed)
     }
 }
+
+impl<TLiteral: Debug + Clone + Eq + Hash + Display> Display for Expression<TLiteral> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        match self {
+            Constant(value) => write!(f, "{}", value),
+            Literal(name) => write!(f, "{}", name),
+            Not(inner) => write!(f, "!{}", inner),
+            And(expressions) | Or(expressions) => write!(
+                f,
+                "({})",
+                expressions
+                    .iter()
+                    .fold(String::new(), |acc, elem| format!("{acc} & {elem}"))
+            ),
+        }
+    }
+}

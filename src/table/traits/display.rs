f3640789185cfb6ad5d9e92e99407cb9ff4ace6f
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-12 16:43:16 +0200

    feat: added test case for display

--- src/table/traits/display.rs
+++ src/table/traits/display.rs
@@ -26,7 +26,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_output_and_ok() {
+    fn test_output_short_variables_and_ok() {
         let input_expression = Expression::binary_and(Literal("x0"), Literal("x1"));
         let input_table = TruthTable::from(input_expression.clone());
 
@@ -36,6 +36,24 @@ mod tests {
             "false true  false  \n",
             "true  false false  \n",
             "true  true  true   ",
+        );
+        let actual = input_table.to_string();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_output_long_variables_and_ok() {
+        let input_expression =
+            Expression::binary_and(Literal("longvariablename1"), Literal("longvariablename2"));
+        let input_table = TruthTable::from(input_expression.clone());
+
+        let expected = concat!(
+            "longvariablename1 longvariablename2 result \n",
+            "false             false             false  \n",
+            "false             true              false  \n",
+            "true              false             false  \n",
+            "true              true              true   ",
         );
         let actual = input_table.to_string();
 

6d1e006de78e04087813aa7b5a6c85f6131ead01
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-12 10:58:58 +0200

    feat!: implemented TruthTable as a struct of variables and bool. function outputs

---
+++ src/table/utils/mod.rs
@@ -0,0 +1,5 @@
+pub use row_index_to_valuation::row_index_to_valuation;
+pub use valuation_to_row_index::{values_to_row_index, values_to_row_index_checked};
+
+mod row_index_to_valuation;
+mod valuation_to_row_index;
---
+++ src/table/utils/row_index_to_valuation.rs
@@ -0,0 +1,17 @@
+pub fn row_index_to_valuation(mut row_index: usize, min_target_len: usize) -> Vec<bool> {
+    let mut result = vec![];
+
+    while row_index > 0 {
+        let digit = row_index.rem_euclid(2);
+        row_index /= 2;
+        result.push(digit != 0);
+    }
+
+    while result.len() < min_target_len {
+        result.push(false);
+    }
+
+    result.reverse();
+
+    result
+}

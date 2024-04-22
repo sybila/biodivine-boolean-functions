e72c6eea01bf6465b89de717cc22f12ebb5be593
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-22 16:05:00 +0200

    feat!: added PythonTruthTable to bindings

---
+++ src/bindings/table.rs
@@ -0,0 +1,100 @@
+use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
+
+use pyo3::PyResult;
+
+use crate::bindings::error::PythonExpressionError::UnknownVariableWhileEvaluating;
+use crate::bindings::expression::PythonExpression;
+use crate::expressions::Expression as RustExpression;
+use crate::table::display_formatted::{TableBooleanFormatting, TableStyle};
+use crate::table::TruthTable;
+use crate::traits::{Evaluate, GatherLiterals, SemanticEq};
+
+#[pyo3::pyclass(frozen)]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PythonTruthTable {
+    root: Arc<TruthTable<String>>,
+}
+
+#[pyo3::pymethods]
+impl PythonTruthTable {
+    #[staticmethod]
+    pub fn from_expression(expression: &PythonExpression) -> Self {
+        let rust_expression: RustExpression<String> = expression.into();
+        let rust_table: TruthTable<String> = rust_expression.into();
+
+        Self::new(rust_table)
+    }
+
+    pub fn to_expression_trivial(&self) -> PythonExpression {
+        self.root.to_expression_trivial().into()
+    }
+
+    pub fn to_string_formatted(
+        &self,
+        style: TableStyle,
+        boolean_formatting: TableBooleanFormatting,
+    ) -> String {
+        self.root
+            .to_string_formatted(style, boolean_formatting, boolean_formatting)
+    }
+
+    pub fn gather_literals(&self) -> HashSet<String> {
+        self.root.gather_literals()
+    }
+
+    /// Throws a `KeyError` when a variable is encountered that isn't found among
+    /// the given `literal_values`.
+    pub fn evaluate_nonsafe(&self, literal_values: HashMap<String, bool>) -> PyResult<bool> {
+        Ok(self
+            .root
+            .evaluate_with_err(&literal_values)
+            .map_err(|name| UnknownVariableWhileEvaluating { name })?)
+    }
+
+    /// Variables not in the dictionary default to false.
+    pub fn evaluate_safe(&self, literal_values: HashMap<String, bool>) -> PyResult<bool> {
+        Ok(self.root.evaluate(&literal_values))
+    }
+
+    pub fn semantic_eq(&self, other: &Self) -> bool {
+        self.root.semantic_eq(&other.root)
+    }
+
+    pub fn row(&self, row_index: usize) -> Vec<bool> {
+        self.root.row(row_index)
+    }
+
+    #[cfg(feature = "csv")]
+    #[staticmethod]
+    pub fn from_csv_file(path: &str) -> PyResult<Self> {
+        Ok(Self::new(TruthTable::from_csv_file(path)?))
+    }
+
+    #[cfg(feature = "csv")]
+    #[staticmethod]
+    pub fn from_csv_string(path: &str) -> PyResult<Self> {
+        Ok(Self::new(TruthTable::from_csv_string(path)?))
+    }
+
+    #[cfg(feature = "csv")]
+    pub fn to_csv(&self) -> String {
+        self.root.to_csv()
+    }
+
+    pub fn __str__(&self) -> String {
+        self.root.to_string()
+    }
+
+    pub fn __repr__(&self) -> String {
+        format!("PythonTruthTable({})", self.__str__())
+    }
+}
+
+impl PythonTruthTable {
+    fn new(table: TruthTable<String>) -> Self {
+        PythonTruthTable {
+            root: Arc::new(table),
+        }
+    }
+}

bfdbebed7f1c6f55249c09d3b799f5e4a71ba652
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-22 16:05:00 +0200

    feat!: added PythonTruthTable to bindings

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -1,7 +1,9 @@
 mod error;
 mod expression;
+mod table;
 
 use crate::bindings::expression::PythonExpression;
+use crate::bindings::table::PythonTruthTable;
 use pyo3::prelude::*;
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -10,5 +12,6 @@ use pyo3::prelude::*;
 #[pymodule]
 fn biodivine_boolean_functions(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PythonExpression>()?;
+    m.add_class::<PythonTruthTable>()?;
     Ok(())
 }

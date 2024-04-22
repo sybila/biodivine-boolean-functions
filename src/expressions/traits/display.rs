f900a68a5d3908ff48647dee4352aa263283e4ec
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-22 20:22:31 +0200

    fix(expression): fixed Display for And or Or expression
    With test.

--- src/expressions/traits/display.rs
+++ src/expressions/traits/display.rs
@@ -1,5 +1,6 @@
 use crate::expressions::Expression;
 use crate::expressions::ExpressionNode::{And, Constant, Literal, Not, Or};
+use itertools::Itertools;
 use std::fmt::{Debug, Display, Error, Formatter};
 use std::hash::Hash;
 
@@ -9,13 +10,33 @@ impl<TLiteral: Debug + Clone + Eq + Hash + Display> Display for Expression<TLite
             Constant(value) => write!(f, "{}", value),
             Literal(name) => write!(f, "{}", name),
             Not(inner) => write!(f, "!({})", inner),
-            And(expressions) | Or(expressions) => write!(
-                f,
-                "({})",
-                expressions
-                    .iter()
-                    .fold(String::new(), |acc, elem| format!("{acc} & {elem}"))
-            ),
+            And(expressions) => Self::fmt_nary_expression(f, expressions, '&'),
+            Or(expressions) => Self::fmt_nary_expression(f, expressions, '|'),
         }
+    }
+}
+
+impl<TLiteral: Debug + Clone + Eq + Hash + Display> Expression<TLiteral> {
+    fn fmt_nary_expression(
+        f: &mut Formatter,
+        expressions: &[Expression<TLiteral>],
+        operator: char,
+    ) -> Result<(), Error> {
+        write!(f, "({})", expressions.iter().join(&format!(" {operator} ")))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::expressions::{bool, var};
+
+    #[test]
+    fn test_display_ok() {
+        let input = var("a") & !var("b") | bool(true);
+
+        let actual = input.to_string();
+        let expected = "((a & !(b)) | true)";
+
+        assert_eq!(actual, expected)
     }
 }

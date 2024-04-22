903c8a004405e415b6ba1f415f35fd1dea62230f
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-22 19:37:37 +0200

    feat(test): added valuation_to_row_index tests to increase coverage

--- src/table/utils/valuation_to_row_index.rs
+++ src/table/utils/valuation_to_row_index.rs
@@ -67,3 +67,113 @@ fn values_to_row_index_common<TLiteral: Debug + Clone + Eq + Hash + Ord>(
 
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    fn test_common_nodefault_unknownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true)]);
+        let default = None;
+
+        let (_index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(!errors.is_empty());
+        assert_eq!(errors, vec!["2"]);
+    }
+
+    #[rstest]
+    fn test_common_defaultfalse_unknownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true)]);
+        let default = Some(false);
+
+        let expected_index = 1usize << 1;
+
+        let (index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(errors.is_empty());
+        assert_eq!(index, expected_index);
+    }
+
+    #[rstest]
+    fn test_common_defaulttrue_unknownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true)]);
+        let default = Some(true);
+
+        let expected_index = (1usize << 1) + (1usize << 0);
+
+        let (index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(errors.is_empty());
+        assert_eq!(index, expected_index);
+    }
+
+    #[rstest]
+    fn test_common_nodefault_knownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true), ("2", true)]);
+        let default = None;
+
+        let expected_index = (1usize << 1) + (1usize << 0);
+
+        let (index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(errors.is_empty());
+        assert_eq!(index, expected_index);
+    }
+
+    #[rstest]
+    fn test_common_defaultfalse_knownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true), ("2", true)]);
+        let default = Some(false);
+
+        let expected_index = (1usize << 1) + (1usize << 0);
+
+        let (index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(errors.is_empty());
+        assert_eq!(index, expected_index);
+    }
+
+    #[rstest]
+    fn test_common_defaulttrue_knownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true), ("2", false)]);
+        let default = Some(true);
+
+        let expected_index = 1usize << 1;
+
+        let (index, errors) = values_to_row_index_common(&order, &valuation, default);
+
+        assert!(errors.is_empty());
+        assert_eq!(index, expected_index);
+    }
+
+    #[rstest]
+    fn test_checked_unknownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true)]);
+
+        let actual = values_to_row_index_checked(&order, &valuation);
+        let expected = Err(vec!["2"]);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[rstest]
+    fn test_checked_knownvar() {
+        let order = vec!["1", "2"];
+        let valuation = HashMap::from_iter(vec![("1", true), ("2", true)]);
+
+        let actual = values_to_row_index_checked(&order, &valuation);
+        let expected = Ok((1usize << 1) + (1usize << 0));
+
+        assert_eq!(actual, expected);
+    }
+}

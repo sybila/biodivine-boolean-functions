1d90d02561ac5f570939636cf871cf0d53ce92dc
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-04-27 10:41:31 +0200

    feat(tests): added basic test for is_implied_by for Expression

--- src/expressions/traits/boolean_function.rs
+++ src/expressions/traits/boolean_function.rs
@@ -411,4 +411,12 @@ mod tests {
         assert_eq!(evaluated_with_true, evaluated_with_false);
         assert!(evaluated_with_true)
     }
+
+    #[test]
+    fn test_is_implied_by_unit_ok() {
+        assert!(bool(false).is_implied_by(&bool(false)));
+        assert!(bool(true).is_implied_by(&bool(false)));
+        assert!(!bool(false).is_implied_by(&bool(true)));
+        assert!(bool(true).is_implied_by(&bool(true)));
+    }
 }

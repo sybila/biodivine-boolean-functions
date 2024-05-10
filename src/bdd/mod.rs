e7df09271f07056e02d289541c7ce8c5a75a2009
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-10 11:33:07 +0200

    feat(bdd): expanded doc comments

--- src/bdd/mod.rs
+++ src/bdd/mod.rs
@@ -30,13 +30,17 @@ pub struct Bdd<TLiteral>
 where
     TLiteral: Debug + Clone + Eq + Ord,
 {
-    // Invariant 1: no more than 65k variables (see lib-bdd)
-    // Invariant 2: The input variables are sorted.
+    /// Always-sorted vector of no more than 65k variables (see `lib-bdd`).
     inputs: Vec<TLiteral>,
+    /// Holds the `lib_bdd` representation.
     bdd: InnerBdd,
 }
 
 impl<TLiteral: Debug + Clone + Eq + Ord + 'static> Bdd<TLiteral> {
+    /// Converts a literal representation as a generic user struct
+    /// into `BddVariable` used by `lib_bdd::Bdd`.
+    ///
+    /// If such a variable isn't used in this `Bdd`, the method returns `None`.
     fn map_var_outer_to_inner(&self, variable: &TLiteral) -> Option<BddVariable> {
         self.inputs
             .binary_search(variable)
@@ -44,12 +48,19 @@ impl<TLiteral: Debug + Clone + Eq + Ord + 'static> Bdd<TLiteral> {
             .map(|it| BddVariable::from_index(it))
     }
 
+    /// Converts a `BddVariable` used by `lib_bdd::Bdd` into a literal representation
+    /// used by `self`.
+    ///
+    /// If such a variable isn't used in this `Bdd`, the method returns `None`.
     fn map_var_inner_to_outer(&self, variable: BddVariable) -> Option<TLiteral> {
         self.inputs.get(variable.to_index()).cloned()
     }
 
+    /// Creates a `BddVariableSet` used by `lib_bdd::Bdd`.
+    ///
+    /// Since `lib_bdd` only supports up to 2<sup>16</sup> variables, this method currently panics.
     fn make_inner_variable_set(variables: Vec<TLiteral>) -> BddVariableSet {
-        let num_vars = u16::try_from(variables.len()).expect("Too many variables"); // TODO: Error here?
+        let num_vars = u16::try_from(variables.len()).expect("Too many variables");
         BddVariableSet::new_anonymous(num_vars)
     }
 }

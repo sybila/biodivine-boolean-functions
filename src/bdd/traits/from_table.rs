fa826a1037d001947e04de0fe578bcb2d08669ca
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-13 11:10:59 +0200

    feat(tests): bdd_from_table

--- src/bdd/traits/from_table.rs
+++ src/bdd/traits/from_table.rs
@@ -31,3 +31,36 @@ impl<T: Debug + Clone + Ord> TryFrom<TruthTable<T>> for Bdd<T> {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::expressions::Expression;
+    use itertools::Itertools;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_bdd_from_table_too_many_variables_nok() {
+        let disallowed_count = u16::MAX as usize + 1;
+        let inputs = (0..disallowed_count).collect_vec();
+        // not accurate but doesn't effect test result
+        let outputs = vec![true; 2usize.pow(16)];
+
+        let table = TruthTable::new(inputs, outputs);
+        let bdd = Bdd::try_from(table);
+
+        assert!(bdd.is_err());
+    }
+
+    #[test]
+    fn test_bdd_from_table_ok() {
+        let exp_string = "(b | a & c & false) & !a | true".to_string();
+        let source = Expression::from_str(&exp_string).unwrap();
+        let expected = Bdd::try_from(source.clone()).unwrap();
+
+        let input = TruthTable::from(source);
+        let actual = Bdd::try_from(input).unwrap();
+
+        assert!(actual.is_equivalent(&expected));
+    }
+}

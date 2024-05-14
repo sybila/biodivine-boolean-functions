4958293fa206c2196be625ab5ac3141dd4f2d8e4
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-14 21:19:46 +0200

    fix(bdd): uncommented test that was off by mistake

--- src/bdd/iterators/support.rs
+++ src/bdd/iterators/support.rs
@@ -20,72 +20,71 @@ impl Iterator for SupportIterator {
     }
 }
 
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::expressions::var;
-//     use crate::traits::BooleanFunction;
-//     use std::collections::BTreeMap;
-//
-//     #[test]
-//     fn test_image_ok() {
-//         let input = var("d") & var("b") | var("a");
-//
-//         let mut actual = input.image();
-//         let expected = [
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), false),
-//                 ("b".to_string(), false),
-//                 ("d".to_string(), false),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), false),
-//                 ("b".to_string(), false),
-//                 ("d".to_string(), true),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), false),
-//                 ("b".to_string(), true),
-//                 ("d".to_string(), false),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), false),
-//                 ("b".to_string(), true),
-//                 ("d".to_string(), true),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), true),
-//                 ("b".to_string(), false),
-//                 ("d".to_string(), false),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), true),
-//                 ("b".to_string(), false),
-//                 ("d".to_string(), true),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), true),
-//                 ("b".to_string(), true),
-//                 ("d".to_string(), false),
-//             ]))),
-//             Some(input.evaluate(&BTreeMap::from([
-//                 ("a".to_string(), true),
-//                 ("b".to_string(), true),
-//                 ("d".to_string(), true),
-//             ]))),
-//         ];
-//
-//         assert_eq!(actual.next(), expected[0]);
-//         assert_eq!(actual.next(), expected[1]);
-//         assert_eq!(actual.next(), expected[2]);
-//         assert_eq!(actual.next(), expected[3]);
-//         assert_eq!(actual.next(), expected[4]);
-//         assert_eq!(actual.next(), expected[5]);
-//         assert_eq!(actual.next(), expected[6]);
-//         assert_eq!(actual.next(), expected[7]);
-//
-//         assert_eq!(actual.next(), None);
-//         assert_eq!(actual.next(), None);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::bdd::Bdd;
+    use crate::expressions::var;
+    use crate::traits::{BooleanFunction, Evaluate};
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test_image_ok() {
+        let input = Bdd::try_from(var("d") & var("b") | var("a")).expect("Should not panic here");
+
+        let mut actual = input.image();
+        let expected = [
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), false),
+                ("b".to_string(), false),
+                ("d".to_string(), false),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), false),
+                ("b".to_string(), false),
+                ("d".to_string(), true),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), false),
+                ("b".to_string(), true),
+                ("d".to_string(), false),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), false),
+                ("b".to_string(), true),
+                ("d".to_string(), true),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), true),
+                ("b".to_string(), false),
+                ("d".to_string(), false),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), true),
+                ("b".to_string(), false),
+                ("d".to_string(), true),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), true),
+                ("b".to_string(), true),
+                ("d".to_string(), false),
+            ]))),
+            Some(input.evaluate(&BTreeMap::from([
+                ("a".to_string(), true),
+                ("b".to_string(), true),
+                ("d".to_string(), true),
+            ]))),
+        ];
+
+        assert_eq!(actual.next(), expected[0]);
+        assert_eq!(actual.next(), expected[1]);
+        assert_eq!(actual.next(), expected[2]);
+        assert_eq!(actual.next(), expected[3]);
+        assert_eq!(actual.next(), expected[4]);
+        assert_eq!(actual.next(), expected[5]);
+        assert_eq!(actual.next(), expected[6]);
+        assert_eq!(actual.next(), expected[7]);
+
+        assert_eq!(actual.next(), None);
+        assert_eq!(actual.next(), None);
+    }
+}

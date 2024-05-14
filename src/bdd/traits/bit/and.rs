b69b4b1df316e4572e084be4496cb163d3833fae
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-14 23:02:01 +0200

    feat(tests): added bdd.and tests

--- src/bdd/traits/bit/and.rs
+++ src/bdd/traits/bit/and.rs
@@ -11,3 +11,80 @@ impl<TLiteral: Debug + Clone + Eq + Ord + 'static> BitAnd for Bdd<TLiteral> {
         bit_common(self, rhs, InnerBdd::and)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::bdd::Bdd;
+    use crate::expressions::var;
+    use crate::table::TruthTable;
+    use crate::traits::{BooleanFunction, Implication};
+
+    #[test]
+    fn test_and_same_variables() {
+        let and = var("a") & var("b");
+        let imply = var("a").imply(var("b"));
+
+        let and_table = Bdd::try_from(and.clone()).expect("Should not panic here");
+        let imply_table = Bdd::try_from(imply.clone()).expect("Should not panic here");
+
+        let actual = and_table & imply_table;
+        let expected = Bdd::try_from(and & imply).expect("Should not panic here");
+
+        assert!(actual.is_equivalent(&expected));
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_and_different_variables_1() {
+        let and = var("a") & var("b");
+        let imply = var("b").imply(var("c"));
+
+        let and_table = Bdd::try_from(and.clone()).expect("Should not panic here");
+        let imply_table = Bdd::try_from(imply.clone()).expect("Should not panic here");
+
+        let actual = and_table & imply_table;
+        let expected = Bdd::try_from(and & imply).expect("Should not panic here");
+
+        assert!(actual.is_equivalent(&expected));
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_and_different_variables_2() {
+        let and = var("c") & var("b");
+        let imply = var("a").imply(var("d"));
+
+        let and_table = Bdd::try_from(and.clone()).expect("Should not panic here");
+        let imply_table = Bdd::try_from(imply.clone()).expect("Should not panic here");
+
+        let actual = and_table & imply_table;
+        let expected = Bdd::try_from(and & imply).expect("Should not panic here");
+
+        assert!(actual.is_equivalent(&expected));
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_and_different_variables_3() {
+        let lhs = Bdd::try_from(TruthTable::new(
+            vec!["a", "b"],
+            vec![true, true, false, true],
+        ))
+        .expect("Should not panic here");
+        let rhs = Bdd::try_from(TruthTable::new(
+            vec!["a", "c"],
+            vec![false, true, false, true],
+        ))
+        .expect("Should not panic here");
+
+        let actual = lhs & rhs;
+        let expected = Bdd::try_from(TruthTable::new(
+            vec!["a", "b", "c"],
+            vec![false, true, false, true, false, false, false, true],
+        ))
+        .expect("Should not panic here");
+
+        assert!(actual.is_equivalent(&expected));
+        assert_eq!(actual, expected);
+    }
+}

a21b532e090d3831972559db6b597407666facd0
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-20 17:40:35 +0200

    feat(expression): added semantic test for Implication

--- src/expressions/traits/operations/implication.rs
+++ src/expressions/traits/operations/implication.rs
@@ -12,16 +12,28 @@ impl<T: Debug + Clone + Eq + Ord> Implication<Expression<T>> for Expression<T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::expressions::var;
+    use crate::expressions::{bool, var};
     use crate::traits::{Implication, SemanticEq};
 
     #[test]
-    fn test_imply_ok() {
+    fn test_imply_syntactic_ok() {
         let actual = var("a").imply(var("b"));
         let expected_alternative_implication = !(var("a") & !var("b"));
         let expected_actual_implication = !var("a") | var("b");
 
         assert!(actual.semantic_eq(&expected_alternative_implication));
         assert_eq!(actual, expected_actual_implication);
+    }
+
+    #[test]
+    fn test_imply_semantic_ok() {
+        assert!(bool(false).imply(bool(false)).semantic_eq(&bool(true)));
+        assert!(bool(false).imply(bool(true)).semantic_eq(&bool(true)));
+        assert!(bool(true).imply(bool(false)).semantic_eq(&bool(false)));
+        assert!(bool(true).imply(bool(true)).semantic_eq(&bool(true)));
+
+        assert!(var("x").imply(bool(false)).semantic_eq(&!var("x")));
+        assert!(bool(true).imply(var("x")).semantic_eq(&var("x")));
+        assert!(!var("x").imply(var("x")).semantic_eq(&var("x")));
     }
 }

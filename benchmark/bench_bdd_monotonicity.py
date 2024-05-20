338abc0ffdaca6a632875b2120ab436d61aee397
AurumTheEnd <47597303+aurumtheend@users.noreply.github.com>
2024-05-20 22:40:18 +0200

    refactor(bench): fixed pep8 errors apart from long line lengths

--- benchmark/bench_bdd_monotonicity.py
+++ benchmark/bench_bdd_monotonicity.py
@@ -3,87 +3,89 @@ from pyeda.inter import *
 from utils import run_bench
 
 """
-This benchmark constructs a BDD representing the set of all monotonic 
+This benchmark constructs a BDD representing the set of all monotonic
 functions of the given arity. The size of this set are the "Dedekind
 numbers". Computing dedekind numbers is an open problem with no known
 solution better than O((n ** 2) ** 2) (i.e. double exponential).
 
 Regardless of variable ordering, the BDD for this problem is exponentially
-large. Furthermore, this problem is biologically relevant, since the 
+large. Furthermore, this problem is biologically relevant, since the
 Boolean functions that typically appear in biological models are assumed
 to be monotonic.
 """
 
+
 def var_name(var_id):
-	# These weird names ensure that (at least initially),
-	# the variables are sorted based on their ID.
-	return "v_" + ("x" * var_id)
+    # These weird names ensure that (at least initially),
+    # the variables are sorted based on their ID.
+    return "v_" + ("x" * var_id)
+
 
 def bench_monotonicity_bbf(arity):
-    num_vars = 2 ** arity    
+    num_vars = 2 ** arity
 
-    variables = [ var_name(i) for i in range(num_vars) ]
-    literals = [ bbf.Bdd.mk_literal(var, True) for var in variables ]
+    variables = [var_name(i) for i in range(num_vars)]
+    literals = [bbf.Bdd.mk_literal(var, True) for var in variables]
     result = bbf.Bdd.mk_const(True)
 
     for i in range(arity):
-    	block_size = 2 ** (i + 1)
-    	half_block = int(block_size / 2)
-    	regulator_formula = bbf.Bdd.mk_const(True)
-    	for block in range(int(num_vars / block_size)):
-    		for block_item in range(half_block):
-    			var1 = literals[block_size * block + block_item]
-    			var2 = literals[block_size * block + block_item + half_block]
-    			implies = bbf.Bdd.mk_or(bbf.Bdd.mk_not(var1), var2)
-    			regulator_formula = bbf.Bdd.mk_and(regulator_formula, implies)
-    	result = bbf.Bdd.mk_and(result, regulator_formula)    
+        block_size = 2 ** (i + 1)
+        half_block = int(block_size / 2)
+        regulator_formula = bbf.Bdd.mk_const(True)
+        for block in range(int(num_vars / block_size)):
+            for block_item in range(half_block):
+                var1 = literals[block_size * block + block_item]
+                var2 = literals[block_size * block + block_item + half_block]
+                implies = bbf.Bdd.mk_or(bbf.Bdd.mk_not(var1), var2)
+                regulator_formula = bbf.Bdd.mk_and(regulator_formula, implies)
+        result = bbf.Bdd.mk_and(result, regulator_formula)
     return result
+
 
 def bench_monotonicity_pyeda(arity):
-    num_vars = 2 ** arity    
-    
-    variables = [ var_name(i) for i in range(num_vars) ]
-    literals = [ bddvar(var) for var in variables ]
+    num_vars = 2 ** arity
+
+    variables = [var_name(i) for i in range(num_vars)]
+    literals = [bddvar(var) for var in variables]
     result = 1
-        
+
     for i in range(arity):
-    	block_size = 2 ** (i + 1)
-    	half_block = int(block_size / 2)
-    	regulator_formula = 1
-    	for block in range(int(num_vars / block_size)):
-    		for block_item in range(half_block):
-    			var1 = literals[block_size * block + block_item]
-    			var2 = literals[block_size * block + block_item + half_block]
-    			implies = ~var1 | var2
-    			regulator_formula = regulator_formula & implies
-    	result = result & regulator_formula
+        block_size = 2 ** (i + 1)
+        half_block = int(block_size / 2)
+        regulator_formula = 1
+        for block in range(int(num_vars / block_size)):
+            for block_item in range(half_block):
+                var1 = literals[block_size * block + block_item]
+                var2 = literals[block_size * block + block_item + half_block]
+                implies = ~var1 | var2
+                regulator_formula = regulator_formula & implies
+        result = result & regulator_formula
     return result
 
+
 header = [
-	"Var. count",
-	"BBF[avg]",
-	"PyEDA[avg]",
-	"BBF[dev]",
-	"PyEDA[dev]",
-	"BBF[times]",
-	"PyEDA[times]"
+    "Var. count",
+    "BBF[avg]",
+    "PyEDA[avg]",
+    "BBF[dev]",
+    "PyEDA[dev]",
+    "BBF[times]",
+    "PyEDA[times]"
 ]
 print("\t".join(header))
 
-for num_vars in range(1,6):	
-	
-	(bbf_avg, bbf_dev, bbf_times) = run_bench(lambda: bench_monotonicity_bbf(num_vars))
-	(pyeda_avg, pyeda_dev, pyeda_times) = run_bench(lambda: bench_monotonicity_pyeda(num_vars))
+for num_vars in range(1, 6):
+    (bbf_avg, bbf_dev, bbf_times) = run_bench(lambda: bench_monotonicity_bbf(num_vars))
+    (pyeda_avg, pyeda_dev, pyeda_times) = run_bench(lambda: bench_monotonicity_pyeda(num_vars))
 
-	
-	row = [
-		str(num_vars),
-		str(bbf_avg),
-		str(pyeda_avg),
-		str(bbf_dev),
-		str(pyeda_dev),
-		str(bbf_times),
-		str(pyeda_times)
-	]
+    row = [
+        str(num_vars),
+        str(bbf_avg),
+        str(pyeda_avg),
+        str(bbf_dev),
+        str(pyeda_dev),
+        str(bbf_times),
+        str(pyeda_times)
+    ]
 
-	print("\t".join(row))
+    print("\t".join(row))
--- benchmark/bench_table_subst.py
+++ benchmark/bench_table_subst.py
@@ -5,9 +5,9 @@ from utils import run_bench
 """
 This benchmark compares the performance of the logic table representations
 using a repeated substitution while increasing the variable count of the
-formula. 
+formula.
 
-The operation starts with a formula 'x_0' and it iteratively 
+The operation starts with a formula 'x_0' and it iteratively
 substitutes 'x_i' for '(x_i ^ x_{i+1})'', starting with i = 0. So in the
 end, we obtain a formula (x_0 ^ (x_1 ^ (x_2 ^ ... ( ^ x_n)))).
 
@@ -17,48 +17,49 @@ As such, it should be more complicated than basic operations on tables
 where the arity of the function does not change.
 """
 
+
 def bench_subst_bbf(num_vars):
-	variable_names = [ f"x_{i}" for i in range(num_vars) ]
-	variables = [ bbf.Table.from_expression(bbf.Expression(f"x_{i}")) for i in range(num_vars) ]
-	table = variables[0]
-	for i in range(num_vars - 1):		
-		exp = bbf.Table.mk_xor(variables[i], variables[i+1])
-		table = table.substitute({ variable_names[i]: exp })
-	return table
+    variable_names = [f"x_{i}" for i in range(num_vars)]
+    variables = [bbf.Table.from_expression(bbf.Expression(f"x_{i}")) for i in range(num_vars)]
+    table = variables[0]
+    for i in range(num_vars - 1):
+        exp = bbf.Table.mk_xor(variables[i], variables[i + 1])
+        table = table.substitute({variable_names[i]: exp})
+    return table
+
 
 def bench_subst_pyeda(num_vars):
-	variables = [ ttvar(f"x_{i}") for i in range(num_vars) ]
-	table = variables[0]
-	for i in range(num_vars - 1):		
-		exp = variables[i] ^ variables[i+1]
-		table = table.compose({ variables[i]: exp })
-	return table
+    variables = [ttvar(f"x_{i}") for i in range(num_vars)]
+    table = variables[0]
+    for i in range(num_vars - 1):
+        exp = variables[i] ^ variables[i + 1]
+        table = table.compose({variables[i]: exp})
+    return table
+
 
 header = [
-	"Var. count",
-	"BBF[avg]",
-	"PyEDA[avg]",
-	"BBF[dev]",
-	"PyEDA[dev]",
-	"BBF[times]",
-	"PyEDA[times]"
+    "Var. count",
+    "BBF[avg]",
+    "PyEDA[avg]",
+    "BBF[dev]",
+    "PyEDA[dev]",
+    "BBF[times]",
+    "PyEDA[times]"
 ]
 print("\t".join(header))
 
 for num_vars in range(2, 20):
-	
-	(bbf_avg, bbf_dev, bbf_times) = run_bench(lambda: bench_subst_bbf(num_vars))
-	(pyeda_avg, pyeda_dev, pyeda_times) = run_bench(lambda: bench_subst_pyeda(num_vars))
+    (bbf_avg, bbf_dev, bbf_times) = run_bench(lambda: bench_subst_bbf(num_vars))
+    (pyeda_avg, pyeda_dev, pyeda_times) = run_bench(lambda: bench_subst_pyeda(num_vars))
 
-	
-	row = [
-		str(num_vars),
-		str(bbf_avg),
-		str(pyeda_avg),
-		str(bbf_dev),
-		str(pyeda_dev),
-		str(bbf_times),
-		str(pyeda_times)
-	]
+    row = [
+        str(num_vars),
+        str(bbf_avg),
+        str(pyeda_avg),
+        str(bbf_dev),
+        str(pyeda_dev),
+        str(bbf_times),
+        str(pyeda_times)
+    ]
 
-	print("\t".join(row))
+    print("\t".join(row))
